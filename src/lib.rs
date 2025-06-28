0b9416ba069aa807bbdcce4de70a3d158e1859cb
Daniel Müller <deso@posteo.net>
2025-06-27 21:39:33 -0700

    Integrate user-configuration test into main test suite
    Integrate the user-configuration test into the main test suite. So far
    it had been a separate integration test, because it needed to run in a
    separate process to ensure isolation. With the test-fork crate we have
    the means to run individual unit tests in separate processes, which
    seems more convenient.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2024 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2018-2025 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #![warn(
@@ -527,6 +527,8 @@ mod tests {
 
   use std::mem::align_of;
 
+  use test_fork::fork;
+
 
   /// Exercise the `Debug` representation of various types.
   #[test]
@@ -632,5 +634,36 @@ mod tests {
   #[should_panic(expected = "invalid cursor position")]
   fn reset_panic() {
     Readline::new().reset(CString::new("abc").unwrap(), 4, true);
+  }
+
+  #[fork]
+  #[test]
+  fn with_user_configuration() {
+    extern "C" {
+      fn rl_parse_and_bind(line: *mut c_char) -> c_int;
+    }
+
+    let mut rl = Readline::new();
+
+    // Configure libreadline to accept the character sequence "jk" as an
+    // indication to exit edit mode and switch into vi-movement-mode. Note
+    // that it is of vital importance that this call be made after the
+    // first `Readline` object was created. As part of this call
+    // libreadline seems to be reading its global configuration, and with
+    // that overwrite all customizations made beforehand.
+    let mut line = CString::new("\"jk\": vi-movement-mode")
+      .unwrap()
+      .into_bytes_with_nul();
+
+    let result = unsafe { rl_parse_and_bind(line.as_mut_ptr() as *mut c_char) };
+    assert_eq!(result, 0);
+
+    assert_eq!(rl.feed(b"abjka"), None);
+    assert_eq!(rl.feed(b"\n").unwrap(), CString::new("ab").unwrap());
+
+    rl = Readline::new();
+
+    assert_eq!(rl.feed(b"abjka"), None);
+    assert_eq!(rl.feed(b"\n").unwrap(), CString::new("ab").unwrap());
   }
 }
